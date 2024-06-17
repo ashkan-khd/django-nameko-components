045c7485522611c504dda91371293e0a1735a3e5
Ashkan Khademian <ashkan.khd.q@gmail.com>
2024-06-17 23:14:17 +0330

    feat: add django_components

---
+++ django_components/facade.py
@@ -0,0 +1,22 @@
+from abc import ABC
+
+
+from nameko.rpc import rpc
+from django_nameko import get_pool
+
+from django_components.nameko import DjangoModels
+
+
+class ComponentFacade(ABC):
+    name: str
+    models = DjangoModels()
+
+    @rpc
+    def ping(self, service_name):
+        print(f"ping from {service_name}")
+        return "pong"
+
+    @classmethod
+    def get_instance(cls):
+        with get_pool().next() as pool:
+            return getattr(pool, cls.name)
